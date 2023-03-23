52cebcbdb79c0ad1540fc2c80c12910b9c29e3c3
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-03-23 11:49:57 +0700

    add deleteIncomes module

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -29,3 +29,13 @@ exports.getIncomes = async (req, res) => {
     }
 }
 
+exports.deleteIncome = async (req, res) => {
+    const { id } = req.params;
+    IncomeSchema.findByIdAndDelete(id)
+        .then((income) => {
+            res.status(200).json({ message: 'Income Deleted' })
+        })
+        .catch((error) => {
+            res.status(500).json({ message: "Server Error" })
+        })
+}
