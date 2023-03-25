33aa977b88f88962c47dc8e52980daf8a58af187
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-03-25 11:51:04 +0700

    add getExpenses

--- backend/controllers/expense.js
+++ backend/controllers/expense.js
@@ -22,3 +22,12 @@ exports.addExpense = async (req, res) => {
 }
 
 
+exports.getExpenses = async (req, res) => {
+    try {
+        const incomes = await IncomeSchema.find().sort({ createdAt: -1 })
+        res.status(200).json(incomes)
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" })
+    }
+}
+
