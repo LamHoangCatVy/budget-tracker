121616dd3eacb6ca21675923a7450e17efe9aed9
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-04-02 12:28:15 +0700

    add Dashboard setup

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Dashboard = () => {
+  return (
+    <DashboardStyled>
+    </DashboardStyled>
+  )
+}
+
+const DashboardStyled = styled.div`
+
+`
+export default Dashboard
