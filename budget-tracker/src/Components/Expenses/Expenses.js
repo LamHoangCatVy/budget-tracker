2ec074b48a15cf4ea6409f99f6382bb7542be08e
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-04-16 14:16:19 +0700

    add total expense

--- budget-tracker/src/Components/Expenses/Expenses.js
+++ budget-tracker/src/Components/Expenses/Expenses.js
@@ -1,17 +1,50 @@
-import React from 'react'
-import styled from 'styled-components'
-import { InnerLayout } from '../../styles/Layout'
+import React from "react";
+import styled from "styled-components";
+import { InnerLayout } from "../../styles/Layout";
+import { useGlobalContext } from "../../context/globalContext";
 
 const Expenses = () => {
+  const { totalExpense } = useGlobalContext();
   return (
     <ExpensesStyled>
-        <InnerLayout>
-            <h1>Expenses</h1>
-        </InnerLayout>
+      <InnerLayout>
+        <h1>Expenses</h1>
+        <h2 className="total-income">
+          Total Expense <span>${totalExpense()}</span>
+        </h2>
+      </InnerLayout>
     </ExpensesStyled>
-  )
-}
+  );
+};
 
 const ExpensesStyled = styled.div`
-`
-export default Expenses
+  display: flex;
+  overflow: auto;
+  .total-income {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #fcf6f9;
+    border: 2px solid #ffffff;
+    box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+    border-radius: 20px;
+    padding: 1rem;
+    margin: 1rem 0;
+    font-size: 2rem;
+    gap: 0.5rem;
+    span {
+      font-size: 2.5rem;
+      font-weight: 800;
+      color: var(--color-green);
+    }
+  }
+
+  .income-content {
+    display: flex;
+    gap: 2rem;
+    .incomes {
+      flex: 1;
+    }
+  }
+`;
+export default Expenses;
