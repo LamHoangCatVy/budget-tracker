eef588451c196cef28773074373ab530610057d9
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-04-17 19:49:01 +0700

    fixed expense backend

--- backend/controllers/expense.js
+++ backend/controllers/expense.js
@@ -1,9 +1,9 @@
 const ExpenseSchema = require("../models/ExpenseSchema")
 
 exports.addExpense = async (req, res) => {
-    const { title, amount, category, descripton, date } = req.body
-    const income = IncomeSchema({
-        title, amount, category, descripton, date
+    const { title, amount, category, description, date } = req.body
+    const expense = ExpenseSchema({
+        title, amount, category, description, date
     })
     try {
         //validations
@@ -13,19 +13,19 @@ exports.addExpense = async (req, res) => {
         if (amount <= 0 || !amount === 'number') {
             return res.status(400).json({ message: 'Amount must be a positive number' })
         }
-        await income.save()
+
+        await expense.save()
         res.status(200).json({ message: "Expense added" })
     } catch (error) {
-
-        res.status(500).json({ message: "Server Error" })
+        res.status(500).json({ message: "Server Errorrrrr" })
     }
 }
 
 
 exports.getExpenses = async (req, res) => {
     try {
-        const incomes = await IncomeSchema.find().sort({ createdAt: -1 })
-        res.status(200).json(incomes)
+        const expenses = await ExpenseSchema.find().sort({ createdAt: -1 })
+        res.status(200).json(expenses)
     } catch (error) {
         res.status(500).json({ message: "Server Error" })
     }
@@ -33,8 +33,8 @@ exports.getExpenses = async (req, res) => {
 
 exports.deleteExpense = async (req, res) => {
     const { id } = req.params;
-    IncomeSchema.findByIdAndDelete(id)
-        .then((income) => {
+    ExpenseSchema.findByIdAndDelete(id)
+        .then((expense) => {
             res.status(200).json({ message: 'Expense Deleted' })
         })
         .catch((error) => {
