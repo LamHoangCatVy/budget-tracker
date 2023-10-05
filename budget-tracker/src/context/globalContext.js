19622b77644603ea8c8450a9ffeefa8fe6668d11
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-10-05 21:44:48 +0700

    add BASE_URL

--- budget-tracker/src/context/globalContext.js
+++ budget-tracker/src/context/globalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/";
+const BASE_URL = "https://budget-tracker-gules-omega.vercel.app/";
 
 const GlobalContext = React.createContext();
 
