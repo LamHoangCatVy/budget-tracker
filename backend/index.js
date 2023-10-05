90f359def8368e86892b5beb68a409026d6fa1be
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-10-06 01:20:29 +0700

    test

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 // const express = require("express");
-const cors = require("cors");
 // const app = express();
 // const transactionRouter = require("./routes/transactions");
 // const mongoose = require("mongoose"); // Import mongoose properly
@@ -21,28 +20,28 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors"); 
 
 app.use(express.json());
 mongoose.connect(
   "mongodb+srv://catvyisstudying:VIObLgGMntiEZUR6@cluster0.rb41xr6.mongodb.net/?retryWrites=true&w=majority"
 );
+
 app.use(
   cors({
     origin: ["https://budget-tracker-app-liard.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true,
   })
-  );
-  
-  app.use("/", (req, res) => {
-    res.send("This is the server");
-  });
+);
 
-  app.use("/add-expense", (req, res) => {
-    res.send("This is add-expense");
-  });
-  
+app.get("/", (req, res) => {
+  res.send("This is the server");
+});
 
+app.get("/add-expense", (req, res) => {
+  res.send("This is add-expense");
+});
 
 app.listen(3000, () => {
   console.log("Server is Running");
