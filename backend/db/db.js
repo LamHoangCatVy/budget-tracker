acb293c2304a4c0aef02400ca210e6ea6f56033a
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-10-05 21:51:31 +0700

    config env and mongoose

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,13 +1,16 @@
-const mongoose = require("mongoose")
-
+const mongoose = require("mongoose");
+require("dotenv").config();
 const db = async () => {
-    try {
-        mongoose.set('strictQuery', false)
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log("DB Connected")
-    } catch (error) {
-        console.log("DB Connection ERROR", error.message)
-    }
-}
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB Connected");
+  } catch (error) {
+    console.log("DB Connection ERROR", error.message);
+  }
+};
 
-module.exports = { db }
+module.exports = { db };
