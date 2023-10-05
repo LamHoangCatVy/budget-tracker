00a083c49c2417284c5f7c0e7bfe7547b40063c3
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-10-05 20:17:21 +0700

    fixed server

--- backend/index.js
+++ backend/index.js
@@ -7,31 +7,35 @@ const { get } = require("http");
 const app = express();
 
 require("dotenv").config();
-const PORT = process.env.PORT; // Set a default port if PORT is not defined in .env
+// const PORT = process.env.PORT; // Set a default port if PORT is not defined in .env
 
 // Middleware
 app.use(
-  cors()
+  cors({
+    origin: ["https://budget-tracker-app-liard.vercel.app/"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
 );
 
 app.get("/test", (req, res) => {
-  res.send("Hello")
-  console.log(123)
-})
+  res.send("Hello");
+  console.log(123);
+});
 
 app.use(express.json());
 
 //routes
 readdirSync("./routes").map((route) => {
   app.use("/api/v1", require("./routes/" + route));
-  console.log(route)
+  console.log(route);
 });
 
 const server = () => {
   db();
   app
-    .listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+    .listen(3000, () => {
+      console.log(`Server is running on port`);
     })
     .on("error", (err) => {
       console.error(`Error starting the server: ${err.message}`);
