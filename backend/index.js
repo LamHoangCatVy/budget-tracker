79a9b41081a82faf1cb118142cefca76d10d5de4
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-10-06 01:11:32 +0700

    test

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,42 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const transactionRouter = require("./routes/transactions");
-const mongoose = require("mongoose"); // Import mongoose properly
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+// const transactionRouter = require("./routes/transactions");
+// const mongoose = require("mongoose"); // Import mongoose properly
 
-require("dotenv").config();
-app.use(
-  cors({
-    origin: ["https://budget-tracker-app-liard.vercel.app/"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+// require("dotenv").config();
+// app.use(
+//   cors({
+//     origin: ["https://budget-tracker-app-liard.vercel.app/"],
+//     methods: ["POST", "GET"],
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json());
+// mongoose.connect("mongodb+srv://catvyisstudying:VIObLgGMntiEZUR6@cluster0.rb41xr6.mongodb.net/?retryWrites=true&w=majority");
+
+// // Routes
+// app.use("/api/v1/", transactionRouter);
+
+// // Middleware
+
+// app.listen(3000, () => {
+//   console.log("Server is Running")
+// })
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
 app.use(express.json());
-mongoose.connect("mongodb+srv://catvyisstudying:VIObLgGMntiEZUR6@cluster0.rb41xr6.mongodb.net/?retryWrites=true&w=majority");
-
-// Routes
-app.use("/api/v1/", transactionRouter);
-
-// Middleware
+mongoose.connect(
+  "mongodb+srv://catvyisstudying:VIObLgGMntiEZUR6@cluster0.rb41xr6.mongodb.net/?retryWrites=true&w=majority"
+);
+app.use("/", (req, res) => {
+  res.send("This is the server")
+});
 
 app.listen(3000, () => {
-  console.log("Server is Running")
-})
+  console.log("Server is Running");
+});
