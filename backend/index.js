d5c83eedcbe441135361ef21789a6ad365a23ab3
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-10-05 21:11:37 +0700

    fixed backend

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,12 @@ const transactionRouter = require('./routes/transactions')
 require("dotenv").config();
 const PORT = process.env.PORT || 3000; // Use the provided PORT or default to 3000
 
+// Routes
+app.use('/api/v1/', transactionRouter)
+
 // Middleware
 app.use(express.json());
 
-// Routes
-app.use('/api/v1', transactionRouter)
 
 const server = () => {
   db(); // Make sure to configure your database connection properly
