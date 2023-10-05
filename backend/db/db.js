e8dd2b83c41d379ca1f43fe84b0f21683ac76a54
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-10-05 22:22:18 +0700

    fixed server

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,13 +1,35 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const db = async () => {
-    try {
-        mongoose.set('strictQuery', false)
-        await mongoose.connect("mongodb+srv://catvyisstudying:VIObLgGMntiEZUR6@cluster0.rb41xr6.mongodb.net/?retryWrites=true&w=majority")
-        console.log("DB Connected")
-    } catch (error) {
-        console.log("DB Connection ERROR", error.message)
-    }
-}
+const dbURL = process.env.MONGODB_URI;
 
-module.exports = { db }
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(dbURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    // You can choose to re-throw the error here if needed
+  }
+};
+
+const disconnectFromDatabase = async () => {
+  try {
+    await mongoose.disconnect();
+    console.log("Disconnected from MongoDB");
+  } catch (error) {
+    console.error("Error disconnecting from MongoDB:", error.message);
+  }
+};
+
+// Listen for the 'SIGINT' event (Ctrl+C) to gracefully close the database connection
+process.on("SIGINT", () => {
+  disconnectFromDatabase().then(() => {
+    process.exit(0);
+  });
+});
+
+module.exports = { connectToDatabase, disconnectFromDatabase };
--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,33 @@
-// ../../backend/index.js
-
-const { db } = require("./db/db");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const transactionRouter = require("./routes/transactions");
+const { connectToDatabase } = require("./db/db");
 
 require("dotenv").config();
 
 // Middleware
-app.use(cors({
-  origin: ["https://budget-tracker-app-liard.vercel.app/"],
-  methods: ["POST", "GET"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ["https://budget-tracker-app-liard.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
 app.use("/api/v1/", transactionRouter);
 
+// Hello route for testing
+app.get("/", (req, res) => {
+  res.send("Hello, this is your server!");
+});
+
 const server = async () => {
   try {
     // Ensure the database connection is established before starting the server
-    await db(); // Make sure to configure your database connection properly
+    await connectToDatabase();
     const port = process.env.PORT || 3000;
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
