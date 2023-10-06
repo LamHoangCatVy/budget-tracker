3f1c30f350de04df63d1cf61140334bad1dd4fa6
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-10-06 16:29:24 +0700

    add exact object income property

--- budget-tracker/src/Components/Income/Income.js
+++ budget-tracker/src/Components/Income/Income.js
@@ -7,38 +7,38 @@ import { useEffect } from "react";
 import IncomeItem from "../IncomeItem/IncomeItem";
 
 const Income = () => {
-  const {  incomes, getIncomes, deleteIncome, totalIncome, error } =
+  const { incomes, getIncomes, deleteIncome, totalIncome, error } =
     useGlobalContext();
+
   useEffect(() => {
     getIncomes();
   }, []);
+
   return (
     <IncomeStyled>
       <InnerLayout>
-        <h2 className="total-income">Total Income <span>${totalIncome()}</span></h2>
+        <h2 className="total-income">
+          Total Income <span>${totalIncome()}</span>
+        </h2>
         <div className="income-content">
           <div className="form-container">
             <Form error={error} />
           </div>
           <div className="incomes">
-            {incomes.map((income) => {
-              const { _id, title, amount, date, category, description, type } =
-                income;
-              return (
-                <IncomeItem
-                  key={_id}
-                  id={_id}
-                  title={title}
-                  description={description}
-                  amount={amount}
-                  date={date}
-                  type={type}
-                  category={category}
-                  indicatorColor="var(--color-green)"
-                  deleteItem={deleteIncome}
-                />
-              );
-            })}
+            {incomes.map((income) => (
+              <IncomeItem
+                key={income._id}
+                id={income._id}
+                title={income.title}
+                description={income.description}
+                amount={income.amount}
+                date={income.date}
+                type={income.type}
+                category={income.category}
+                indicatorColor="var(--color-green)"
+                deleteItem={deleteIncome}
+              />
+            ))}
           </div>
         </div>
       </InnerLayout>
