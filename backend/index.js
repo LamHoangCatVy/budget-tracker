77584de618547bd1a21d0183fd96092009fea5db
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-10-06 16:18:29 +0700

    integrate transaction routes into index.js

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,10 @@
-// const express = require("express");
-// const app = express();
-// const transactionRouter = require("./routes/transactions");
-// const mongoose = require("mongoose"); // Import mongoose properly
-
-// require("dotenv").config();
-
-// app.use(express.json());
-// mongoose.connect("mongodb+srv://catvyisstudying:VIObLgGMntiEZUR6@cluster0.rb41xr6.mongodb.net/?retryWrites=true&w=majority");
-
-// // Routes
-// app.use("/api/v1/", transactionRouter);
-
-// // Middleware
-
-// app.listen(3000, () => {
-//   console.log("Server is Running")
-// })
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const cors = require("cors"); 
+const cors = require("cors");
+
+const { addIncome, getIncomes, deleteIncome } = require('./controllers/income')
+const { addExpense, getExpenses, deleteExpense } = require('./controllers/expense')
 
 app.use(express.json());
 mongoose.connect(
@@ -35,13 +19,15 @@ app.use(
   })
 );
 
-app.get("/get-incomes", (req, res) => {
-  res.send("This is the server");
-});
+// Add income routes
+app.post('/api/v1/add-income', addIncome)
+app.get('/api/v1/get-incomes', getIncomes)
+app.delete('/api/v1/delete-income/:id', deleteIncome)
 
-app.get("/add-expense", (req, res) => {
-  res.send("This is add-expense");
-});
+// Add expense routes
+app.post('/api/v1/add-expense', addExpense)
+app.get('/api/v1/get-expenses', getExpenses)
+app.delete('/api/v1/delete-expense/:id', deleteExpense)
 
 app.listen(3000, () => {
   console.log("Server is Running");
