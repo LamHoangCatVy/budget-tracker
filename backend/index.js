3aed5d4b3905c8cc561a7eedf1886ad5cae234fc
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-10-19 20:18:26 +0700

    test

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,10 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader('Access-Control-Allow-Origin', 'https://budget-tracker-app-liard.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Include DELETE in the allowed methods.
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
