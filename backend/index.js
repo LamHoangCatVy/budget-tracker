a7e24bfb12379ea54e785a4c16bebba9e0defc8a
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-11-19 00:34:11 +0700

    Set the coorect CORS configuration

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,17 @@
-require("dotenv").config();
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const userRoutes = require("./routes/user");
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://budget-tracker-app-liard.vercel.app/"); // Remove trailing slash
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://budget-tracker-app-liard.vercel.app"
-  ); // Remove trailing slash
-});
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://budget-tracker-app-liard.vercel.app"
-  ); // Remove trailing slash
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// Set the correct CORS configuration
+app.use(
+  cors({
+    origin: "https://budget-tracker-app-liard.vercel.app", // Remove trailing slash
+    methods: ["POST", "GET", "DELETE"],
+    credentials: true,
+  })
+);
 
 const { addIncome, getIncomes, deleteIncome } = require("./controllers/income");
 const {
@@ -37,17 +22,7 @@ const {
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: ["https://budget-tracker-app-liard.vercel.app/"], // Remove trailing slash
-
-    origin: ["https://budget-tracker-app-liard.vercel.app"], // Remove trailing slash
-    origin: ["https://budget-tracker-app-liard.vercel.app"], // Remove trailing slash
-    methods: ["POST", "GET", "DELETE"],
-    credentials: true,
-  })
-);
-
+// API v1 routes
 app.use("/api/v1", userRoutes);
 
 // Add income routes
@@ -60,14 +35,15 @@ app.post("/api/v1/add-expense", addExpense);
 app.get("/api/v1/get-expenses", getExpenses);
 app.delete("/api/v1/delete-expense/:id", deleteExpense);
 
+// Connect to MongoDB database
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    // listen for requests
+    // Listen for requests on the specified port
     app.listen(process.env.PORT, () => {
-      console.log("connected to db & listening on port", process.env.PORT);
+      console.log("Connected to DB and listening on port", process.env.PORT);
     });
   })
   .catch((error) => {
-    console.log(error);
+    console.error(error);
   });
