3abfb60fe8314fda9d1c510931f70867fc6cfe81
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-11-20 14:34:06 +0700

    update Dashboard.js

--- budget-tracker/src/Components/Dashboard/Dashboard.js
+++ budget-tracker/src/Components/Dashboard/Dashboard.js
@@ -32,21 +32,19 @@ function Dashboard() {
             <div className="amount-con">
               <div className="income">
                 <h2>Total Income</h2>
-                <p>
-                  {dollar} {totalIncome()}
-                </p>
+                <p>{totalIncome()}</p>
               </div>
               <div className="expense">
                 <h2>Total Expense</h2>
-                <p>
-                  {dollar} {totalExpenses()}
-                </p>
+                <p>{totalExpenses()}</p>
               </div>
               <div className="balance">
                 <h2>Total Balance</h2>
-                <p>
-                  {dollar} {totalBalance()}
-                </p>
+                {totalBalance() < 0 ? (
+                  <p style={{ color: "red" }}>{totalBalance()}</p>
+                ) : (
+                  <p>{totalBalance()}</p>
+                )}
               </div>
             </div>
           </div>
@@ -56,15 +54,31 @@ function Dashboard() {
               Min <span>Salary</span>Max
             </h2>
             <div className="salary-item">
-              <p>${Math.min(...incomes.map((item) => item.amount))}</p>
-              <p>${Math.max(...incomes.map((item) => item.amount))}</p>
+              <p>
+                {incomes.length > 0
+                  ? Math.min(...incomes.map((item) => item.amount))
+                  : -Infinity}
+              </p>
+              <p>
+                {incomes.length > 0
+                  ? Math.max(...incomes.map((item) => item.amount))
+                  : Infinity}
+              </p>
             </div>
             <h2 className="salary-title">
               Min <span>Expense</span>Max
             </h2>
             <div className="salary-item">
-              <p>${Math.min(...expenses.map((item) => item.amount))}</p>
-              <p>${Math.max(...expenses.map((item) => item.amount))}</p>
+              <p>
+                {expenses.length > 0
+                  ? Math.min(...expenses.map((item) => item.amount))
+                  : -Infinity}
+              </p>
+              <p>
+                {expenses.length > 0
+                  ? Math.max(...expenses.map((item) => item.amount))
+                  : Infinity}
+              </p>
             </div>
           </div>
         </div>
@@ -80,7 +94,7 @@ const DashboardStyled = styled.div`
     gap: 2rem;
     .chart-con {
       grid-column: 1 / 4;
-      height: 400px;
+      height: 600px;
       .amount-con {
         display: grid;
         grid-template-columns: repeat(4, 1fr);
@@ -90,7 +104,6 @@ const DashboardStyled = styled.div`
         .expense {
           justify-content: center;
           align-items: center;
-
           grid-column: span 2;
         }
         .income,
@@ -100,13 +113,26 @@ const DashboardStyled = styled.div`
           border: 2px solid #ffffff;
           box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
           border-radius: 20px;
-          padding: 1rem;
+          padding: 0.5rem;
+          display: flex;
+          flex-direction: column;
+          h2 {
+            font-size: 1.6rem;
+          }
           p {
-            font-size: 2rem;
+            font-size: 1.6rem;
             font-weight: 700;
+            color: rgba(34, 34, 96, 1);
           }
         }
-
+        .income h2,
+        p {
+          color: var(--color-green);
+        }
+        .expense h2,
+        p {
+          color: red;
+        }
         .balance {
           grid-column: 2 / 4;
           display: flex;
